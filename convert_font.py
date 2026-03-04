1b3e1c4f1358af241c91025b6ddec3a9354b070b
Nikhil6204 <114085464+Nikhil6204@users.noreply.github.com>
2026-03-04 12:34:08 +0530

    Remove sensitive files

--- convert_font.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-with open("./src/fonts/NotoSansDevanagari-Regular.ttf", "rb") as f:
-    data = base64.b64encode(f.read()).decode("utf-8")
-
-with open("./src/fonts/NotoSansDevanagari.js", "w") as f:
-    f.write(f'export const font = "{data}";\n')
-
-print("Done! NotoSansDevanagari.js created successfully.")
